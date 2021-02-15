b3b2c75fc7866a26ed8df82fafb79ac4d451315d
Ashlie Gray <ashliejanegray@gmail.com>
2021-02-16 09:47:49 +1000

    require http/client, getting res status ok from goog.com

--- src/url-checker.cr
+++ src/url-checker.cr
@@ -1,6 +1,9 @@
-# TODO: Write documentation for `Url::Checker`
-module Url::Checker
-  VERSION = "0.1.0"
+# # TODO: Write documentation for `Url::Checker`
+# module Url::Checker
+#   VERSION = "0.1.0"
 
-  # TODO: Put your code here
-end
+#   # TODO: Put your code here
+# end
+require "http/client"
+response = HTTP::Client.get "http://www.google.com"
+p response.status_code
