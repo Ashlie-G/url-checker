92c2d0f5ea0e171f939c7e65405b5e8ab923646a
Ashlie Gray <ashliejanegray@gmail.com>
2021-02-16 12:05:37 +1000

    created proc to read from .yml and print urls as strings in array

--- src/url-checker.cr
+++ src/url-checker.cr
@@ -5,5 +5,18 @@
 #   # TODO: Put your code here
 # end
 require "http/client"
-response = HTTP::Client.get "http://www.google.com"
-p response.status_code
+require "yaml"
+
+# reads over the yml file and creates and array of strings
+# p YAML.parse(File.read("../urls.yml"))["urls"].as_a.map(&.as_s).class
+
+#the above code reafactored to a proc
+get_urls = -> {
+  file_lines = File.read("../urls.yml")
+  YAML.parse(file_lines)["urls"].as_a.map(&.as_s)
+}
+p get_urls.call
+
+#to get google.com using http client
+# response = HTTP::Client.get "http://www.google.com"
+# p response.status_code
