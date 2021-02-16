17edae6fe1aee6a52366faed7ac90aa6e3022fe9
Ashlie Gray <ashliejanegray@gmail.com>
2021-02-16 13:16:54 +1000

    now running concurrently

--- src/url-checker.cr
+++ src/url-checker.cr
@@ -20,14 +20,50 @@ p get_urls.call
 #function to get the status of a url, dont forget to specify the type in the params
 get_status = ->(url : String) {
   begin
+    puts "calling #{url}"
   response = HTTP::Client.get url
   {url, response.status_code}
-  rescue e : Errno | Socket::Addrinfo::Error #in crystal you can specify normal error or specific
+  rescue e : IO::Error | Socket::Addrinfo::Error #in crystal you can specify normal error or specific
     {url, e}
+  ensure
+    puts "called #{url}"
   end
 }
-p get_urls.call #array(string)
-.map(&get_status)
+#create channel to run concurrently (creating the generator and channel)
+# the 'send' is a blocking code, so spawn is needed to await the result
+url_stream = Channel(String).new
+result_stream = Channel({String, Int32 | Exception }).new
+spawn do
+  get_urls.call.each { |url|
+    url_stream.send url
+  }
+end
+
+#create workers
+#loop do within spawn so that each value in the yml is processed rather than just the 2 times specified before the spawn
+2.times {
+  spawn do
+    loop do
+      url = url_stream.receive
+      result = get_status.call(url)
+
+      result_stream.send result
+    end
+  end
+}
+
+#printer
+loop do
+  puts result_stream.receive
+end
+#2 workers but one main channel so that both workers are pushing the result to the channel
+# url_generator -> [url] -> worker_0 -> [{url, result}]
+#                         \_worker_1_/                          
+
+
+#puts get_urls.call #array(string)
+# .map(&get_status).join("\n")
+
 # p get_status(get_urls.call.first)
 #to get google.com using http client
 # response = HTTP::Client.get "http://www.google.com"
